e0a5453fd6b81b3fdf91dc1c2e2082a171959a67
Steve Keeler <github@cairngrove.ca>
2018-03-22 23:39:39 -0400

    Add -Credential parameter to Set-ADUser command or we get insufficient privilege

--- wad-sql-iis-dsc/dsc/DomainMember-IIS.ps1
+++ wad-sql-iis-dsc/dsc/DomainMember-IIS.ps1
@@ -98,7 +98,7 @@ Configuration DomainMember-IIS
 	    {
       	    SetScript = {
                 Add-WindowsFeature -Name "RSAT-AD-PowerShell" 
-                Get-ADUser -Identity $using:Admincreds.UserName | Set-ADUser -ChangePasswordAtLogon $true
+                Get-ADUser -Identity $using:Admincreds.UserName | Set-ADUser -Credential $using:DomainCreds -ChangePasswordAtLogon $true
                 Write-Verbose -Verbose "Configure admin account to force password change at next login" 
             }
             GetScript =  { @{} }
